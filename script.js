206f324a7a5f4660b8263b9d492b874cf50efab9
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-29 05:06:20 +0200

    Created html and js file

---
+++ script.js
@@ -0,0 +1,55 @@
+// scripts.js
+
+const data = {
+  lists: [
+    ["first", [15, 11, 13, 7, 5]],
+    ["second", [2, 6, 8, 4, 14, 12, 10]],
+    ["third", [9, 3, 1]],
+  ],
+};
+
+// Only edit below
+
+const first = data.first[0][1];
+const second = data.second[1][1];
+const third = data.third[2][1];
+
+const result = [];
+
+const extractBiggest = () => {
+  const firstLast = first[first.length - 1] || -Infinity;
+  const secondLast = second[second.length - 1] || -Infinity;
+  const thirdLast = third[third.length - 1] || -Infinity;
+
+  if (firstLast >= secondLast && firstLast >= thirdLast) {
+    return first.pop();
+  }
+
+  if (secondLast >= thirdLast) {
+    return second.pop();
+  }
+
+  return third.pop();
+};
+
+// Only edit above
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+result.push(extractBiggest());
+
+console.log(result);
